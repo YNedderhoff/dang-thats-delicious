aa29078c748ee10651467dba262a8546f17b43d5
yanicknedderhoff <yanick@brandwatch.com>
2018-02-09 15:32:37 +0100

    routing experiments

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+    res.send('Hey! It works!');
+});
+
+router.get('/test/:name/', (req, res) => {
+    const response = {
+        param: req.query.first,
+        param2: req.params.name
+    };
+    console.log("TestLog");
+    res.json(response);
 });
 
 module.exports = router;
