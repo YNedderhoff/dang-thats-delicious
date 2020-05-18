9e282993e8113479b08b83efae249573d7c1c19d
Yanick Nedderhoff <yanicknedderhoff@gmail.com>
2020-05-18 20:42:36 +0200

    Setup basic content template

--- routes/mainController.js
+++ routes/mainController.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.render('hello', {'appName': 'Dang, that\'s delicious!'});
+});
+
+
 module.exports = router;
